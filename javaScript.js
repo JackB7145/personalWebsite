e0f5241e11488f2a9221863b5fbe9207c269b60a
Jack Branston <jbranston6@gmail.com>
2023-09-29 01:08:56 -0400

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,24 +1,7 @@
 // initializing the variables and constants
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzHHIUnWm-ykpqxCEvwrxQuE-JSFqftRX7QJxdSN-by2eUOyV1_gSfjkTWUOZKaZnVVmA/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
 var sizemenu =  document.getElementById("sidemenu")
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
-
-// Listening for the submit button on the google form
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch('https://script.google.com/macros/s/AKfycbzHHIUnWm-ykpqxCEvwrxQuE-JSFqftRX7QJxdSN-by2eUOyV1_gSfjkTWUOZKaZnVVmA/exec', { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        form.reset()
-        msg.innerHTML = "Message Sent Successfully!"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        }, 3000)
-    })
-    .catch(error => console.error('Error!', error.message))
-})
 
 // Initializing the open tab function for the about me section
 function opentab(tabname){
